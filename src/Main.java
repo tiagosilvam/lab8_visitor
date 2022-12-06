5dbf4d22583fbce4b11f4c215c791844f69f9642
Tiago Silva Medeiros <tiagosilvam94@gmail.com>
2022-12-06 10:52:20 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -19,25 +19,27 @@ public class Main {
         Triangulo triangulo1 = new Triangulo(13, 7); // Triangulo sem lado
         Triangulo triangulo2 = new Triangulo(13, 7, 3); // Triangulo com lado
 
-        System.out.println("Cálculo de area:");
-        System.out.printf("Área círculo: %s %n", circulo1.aceitaVisita(visitorArea));
-        System.out.printf("Área retângulo: %s %n", retangulo1.aceitaVisita(visitorArea));
-        System.out.printf("Área trapézio: %s %n", trapezio1.aceitaVisita(visitorArea));
-        System.out.printf("Área triângulo: %s %n", triangulo1.aceitaVisita(visitorArea));
-        System.out.println("\nCálculo de perímetro:");
-        System.out.printf("Perímetro círculo: %s %n", circulo1.aceitaVisita(visitorPerimetro));
-        System.out.printf("Perímetro retângulo: %s %n", retangulo1.aceitaVisita(visitorPerimetro));
-        System.out.printf("Perímetro trapézio: %s %n", trapezio2.aceitaVisita(visitorPerimetro));
-        System.out.printf("Perímetro triângulo: %s %n", triangulo2.aceitaVisita(visitorPerimetro));
-        System.out.println("\nDesenhado:");
+        System.out.println("Cálculo de area das figuras:");
+        System.out.printf("Área círculo: %s cm%n", circulo1.aceitaVisita(visitorArea));
+        System.out.printf("Área retângulo: %s cm%n", retangulo1.aceitaVisita(visitorArea));
+        System.out.printf("Área trapézio: %s cm%n", trapezio1.aceitaVisita(visitorArea));
+        System.out.printf("Área triângulo: %s cm%n", triangulo1.aceitaVisita(visitorArea));
+        System.out.println("\nCálculo de perímetro das figuras:");
+        System.out.printf("Perímetro círculo: %s cm%n", circulo1.aceitaVisita(visitorPerimetro));
+        System.out.printf("Perímetro retângulo: %s cm%n", retangulo1.aceitaVisita(visitorPerimetro));
+        System.out.printf("Perímetro trapézio: %s cm%n", trapezio2.aceitaVisita(visitorPerimetro));
+        System.out.printf("Perímetro triângulo: %s cm%n", triangulo2.aceitaVisita(visitorPerimetro));
+        System.out.println("\nDesenhado figuras:");
         System.out.println(circulo1.aceitaVisita((visitorDesenhar)));
         System.out.println(retangulo1.aceitaVisita((visitorDesenhar)));
         System.out.println(trapezio1.aceitaVisita((visitorDesenhar)));
         System.out.println(triangulo1.aceitaVisita((visitorDesenhar)));
-        System.out.println("\nMaximizado:");
-        System.out.printf("Círculo maximizado: %s %n", circulo1.aceitaVisita((visitorMaximizar)));
-        System.out.printf("Retângulo maximizado: %s %n", retangulo1.aceitaVisita((visitorMaximizar)));
-        System.out.printf("Trapézio maximizado: %s %n", trapezio1.aceitaVisita((visitorMaximizar)));
-        System.out.printf("Triângulo maximizado: %s %n", triangulo1.aceitaVisita((visitorMaximizar)));
+        System.out.println("\nMaximizado figuras:");
+        System.out.printf("Círculo maximizado em cm: %s %n", circulo1.aceitaVisita((visitorMaximizar)));
+        System.out.printf("Retângulo maximizado em cm: %s %n", retangulo1.aceitaVisita((visitorMaximizar)));
+        System.out.printf("Trapézio 1 maximizado em cm: %s %n", trapezio1.aceitaVisita((visitorMaximizar)));
+        System.out.printf("Trapézio 2 maximizado em cm: %s %n", trapezio2.aceitaVisita((visitorMaximizar)));
+        System.out.printf("Triângulo 1 maximizado em cm: %s %n", triangulo1.aceitaVisita((visitorMaximizar)));
+        System.out.printf("Triângulo 2 maximizado em cm: %s %n", triangulo2.aceitaVisita((visitorMaximizar)));
     }
 }
